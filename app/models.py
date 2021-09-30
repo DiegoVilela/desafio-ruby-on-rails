33a168a6b510cc21b5c0f7f8ecd172a7889902b4
Diego Vilela <vilelaphp@gmail.com>
2021-09-30 18:13:13 -0300

    Adiciona os models que serão as bases para o banco de dados

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Shop(models.Model):
+    """Representa uma loja"""
+
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+
+
+class Transaction(models.Model):
+    """Representa um registro financeiro."""
+
+    TYPE = (
+        (1, 'Débito'),
+        (2, 'Boleto'),
+        (3, 'Financiamento'),
+        (4, 'Crédito'),
+        (5, 'Recebimento Empréstimo'),
+        (6, 'Vebdas'),
+        (7, 'Recebimento TED'),
+        (8, 'Recebimento DOC'),
+        (9, 'Aluguel'),
+    )
+    type = models.PositiveSmallIntegerField(
+        choices=TYPE,
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(9)],
+    )
+    date = models.DateField()
+    value = models.IntegerField()
+    cpf = models.IntegerField()
+    time = models.TimeField()
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
