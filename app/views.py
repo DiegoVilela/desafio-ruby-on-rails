ee6ee8c4707edc2a0be924b8480a0cb27b61ad47
Diego Vilela <vilelaphp@gmail.com>
2021-10-01 14:53:28 -0300

    Adiciona o saldo da conta de cada loja

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .forms import UploadFileForm
 from .models import Transaction, Shop
 
 from datetime import datetime
+
+
+SUBTRACTION_TRANSACTION_TYPES = [2, 3, 9]
 
 
 def parse_line(line):
@@ -52,8 +54,16 @@ def upload(request):
 def shop_detail(request, shop_id):
     shop = get_object_or_404(Shop, pk=shop_id)
     transactions = Transaction.objects.filter(shop=shop)
+    balance = 0
+    for transaction in transactions:
+        if transaction.type in SUBTRACTION_TRANSACTION_TYPES:
+            balance -= transaction.value
+        else:
+            balance += transaction.value
     paginator = Paginator(transactions, 20)
+
     return render(request, 'shop_detail.html', {
         'shop': shop,
-        'page_obj': paginator.get_page(request.GET.get('page'))
+        'page_obj': paginator.get_page(request.GET.get('page')),
+        'balance': balance,
     })
