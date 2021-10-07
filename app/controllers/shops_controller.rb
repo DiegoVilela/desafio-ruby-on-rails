57c16d9580ec7b9acdd2743ccf5b256ada124f73
Diego Vilela <vilelaphp@gmail.com>
2021-10-07 18:37:43 -0300

    Inicia o processamento do arquivo de texto

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -42,9 +42,29 @@ class ShopsController < ApplicationController
     redirect_to root_path
   end
 
+  def handle_line(l)
+    if l.size < 81
+      return nil
+    end
+
+    t = Transaction.new
+    t.kind = l[0,1]
+    t.date = Date.strptime(l[1, 8], '%Y%m%d')
+    t.value = l[9,10]
+    t.cpf = l[19,11]
+    t.card = l[30,12]
+    t.time = Time.strptime(l[42,6], "%H%M%S")
+
+    puts t.inspect
+    puts "Nome da Loja: #{l[48,14].strip}"
+    puts "Dono: #{l[62,19].strip}"
+  end
+
   def handle_upload
     uploaded_file = params[:finance]
-    File.foreach(uploaded_file) { |line| puts line }
+    File.foreach(uploaded_file) { |line| handle_line line }
+
+    redirect_to root_path
   end
 
   private
