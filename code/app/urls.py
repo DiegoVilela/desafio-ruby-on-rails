dd2e8476b0b5ee56fe6c11c1839bf0d85c16532d
Diego Vilela <vilelaphp@gmail.com>
2021-10-07 09:41:28 -0300

    Criação da aplicação rails com 'rails new .'

--- code/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.upload, name='index'),
-    path('shop/<int:shop_id>', views.shop_detail, name='shop_detail'),
-]
